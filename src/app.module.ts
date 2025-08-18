2902fe5edad5bf1f8a81ee631d91f5a5f1dedf94
Walter Jose <wrivero1403@gmail.com>
2025-08-19 00:23:58 +0200

    Add configuration and database module imports to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BarsModule } from './bars/bars/bars.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Para que la configuración esté disponible globalmente
+      envFilePath: process.env.NODE_ENV === 'docker' ? '.env.docker' : '.env',
+    }),
+   MongooseModule.forRoot(process.env.MONGO_URI!)
+,
+    BarsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
