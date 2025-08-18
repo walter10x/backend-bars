85441620dca40521e7449247e399c6f57d80e864
Walter Jose <wrivero1403@gmail.com>
2025-08-19 00:24:40 +0200

    Add BarsModule with Mongoose integration for Bar schema

---
+++ src/bars/bars/bars.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BarsService } from './bars.service';
+import { BarsController } from './bars.controller';
+import { Bar, BarSchema } from './bar.schema';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: Bar.name, schema: BarSchema }])],
+  controllers: [BarsController],
+  providers: [BarsService],
+})
+export class BarsModule {}
