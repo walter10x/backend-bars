d3fcb473b5157017d212bb94c6bc10c4e2487f94
Walter Jose <wrivero1403@gmail.com>
2025-08-19 20:46:13 +0200

    Add AuthModule skeleton with commented imports and structure

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,9 @@
+// import { Module } from '@nestjs/common';
+// import { AuthService } from './auth.service';
+// import { Auth\Controller } from './auth/.controller';
+
+// @Module({
+//   providers: [AuthService],
+//   controllers: [Auth\Controller]
+// })
+// export class AuthModule {}
