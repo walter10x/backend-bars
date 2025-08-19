16653d312bc297c18ca0984107c74d6c800bf2ac
Walter Jose <wrivero1403@gmail.com>
2025-08-20 01:24:32 +0200

    Uncomment AuthModule import in app.module.ts to enable authentication features

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BarsModule } from './bars/bars/bars.module';
 import { UsersModule } from './users/users.module';
-//import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -17,7 +17,7 @@ import { UsersModule } from './users/users.module';
 ,
     BarsModule,
     UsersModule,
-    //AuthModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
