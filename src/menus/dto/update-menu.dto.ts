fe089536a75caa7c004d14faf26c46d226987fbd
Walter Jose <wrivero1403@gmail.com>
2025-08-21 23:46:55 +0200

    Add UpdateMenuDto extending CreateMenuDto for partial updates

---
+++ src/menus/dto/update-menu.dto.ts
@@ -0,0 +1,5 @@
+// src/menus/dto/update-menu.dto.ts
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateMenuDto } from './create-menu.dto';
+
+export class UpdateMenuDto extends PartialType(CreateMenuDto) {}
